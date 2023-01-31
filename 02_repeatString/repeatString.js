02a0eb49be5ab991a9a31a65ac92c5ad96ac4f32
cptknvckles <cptknvckles@protonmail.com>
2023-01-31 14:43:32 -0600

    PASSED

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = ''
+    let error = 'ERROR'
+     if(num >= 0){
+        for(let i = 1; i <= num; i++){
+        newString += string
+        }
+      }else{
+        return error
+      }
+    
+    return newString
 };
 
 // Do not edit below this line
